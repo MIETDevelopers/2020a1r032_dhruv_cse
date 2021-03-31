523c7b0d0657125abbb6099b00072aa0b1c346e4
Dhruvgupta <Dhruvgupta2236@gmail.com>
2021-03-31 11:58:13 +0530

    lab 1 experiment

---
+++ addition of two numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  
+int main()
+{
+    int a, b, C;
+    printf("Enter two numbers");
+    scanf("%d%d,"&a,&b);
+    c=a+b
+    printf("sum is %d"',c);
+    return 0;
+}
---
+++ division of two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	
+int a,b,c;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("divide is %d",c);
+return 0;
+}
+
+
